247837d23de79ec810c1e0cb1eec060fc3e2f395
amosbattista <65119558+amosbattista@users.noreply.github.com>
2021-10-21 16:01:44 +0200

    Eliminato Client

--- Client/ConferenceClient/src/Eccezioni/SessionNotPresentException.java
+++
@@ -1,9 +0,0 @@
-package Eccezioni;
-
-public class SessionNotPresentException extends Exception{
-
-    public SessionNotPresentException(String message) {
-        super("Sessione non disponibile. \n"+
-                "Sessioni disponibili: 1~12");
-    }
-}
